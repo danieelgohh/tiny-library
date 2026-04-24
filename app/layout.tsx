b91c0f18a713c9de6cb447002206a8a5be3177ea
Daniel <danieelgohh@hotmail.com>
2026-04-24 15:33:12 +0800

    Implemented layout and homepage

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,33 +1,54 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from "next"
+import { Nunito_Sans, Lato } from "next/font/google"
+import "./globals.css"
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+import Image from "next/image"
+import Link from "next/link"
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+import logo from "@/public/images/tinylibrary-logo.png"
+
+const nunitoSans = Nunito_Sans({
+  variable: "--font-nunito-sans",
   subsets: ["latin"],
-});
+})
+
+const lato = Lato({
+  variable: "--font-lato",
+  subsets: ["latin"],
+  weight: ["700"]
+})
 
 export const metadata: Metadata = {
   title: "Tiny Library",
   description: "A place to store your e-books",
-};
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={`${nunitoSans.variable} ${lato.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <header>
+          <nav className="px-5 py-2.5">
+            <ul className="flex justify-between items-center">
+              <Link href="/">
+                <Image className="w-20 h-12.5 object-cover" src={logo} alt="Tiny library logo" width={200} height={200}></Image>
+              </Link>
+              <div className="flex gap-10 font-semibold font-primary text-[#606060]">
+                <li><Link href="/books">BOOKS</Link></li>
+                <li><Link href="/about">ABOUT</Link></li>
+              </div>
+            </ul>
+          </nav>
+        </header>
+        {children}
+      </body>
     </html>
-  );
+  )
 }
